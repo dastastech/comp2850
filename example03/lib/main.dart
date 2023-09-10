3f78c815d6d1c76d856f9d9b2bb15e1b8ccb2e75
dastastech <dastas.tech@gmail.com>
2023-09-10 13:44:58 -0400

    Change project content.

--- example03/lib/main.dart
+++ example03/lib/main.dart
@@ -4,48 +4,49 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('My Third Flutter App'),
-          ),
-          body: Column(
-            children: <Widget>[
-              const Text('Forma Larga'),
-              const Center(
-                child: Image(
-                  width: 150,
-                  height: 150,
-                  image: NetworkImage(
-                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+        appBar: AppBar(
+          title: const Text('My Third Flutter App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            const Text('Forma Larga'),
+            const Center(
+              child: Image(
+                width: 150,
+                height: 150,
+                image: NetworkImage(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+              ),
+            ),
+            const Center(
+              child: Image(
+                width: 150,
+                height: 150,
+                image: AssetImage(
+                  'images/tiger.png', // Ver el archivo pubspec.yaml
                 ),
               ),
-              const Center(
-                child: Image(
-                  width: 150,
-                  height: 150,
-                  image: AssetImage(
-                    'images/tiger.png', // Ver el archivo pubspec.yaml
-                  ),
-                ),
+            ),
+            const Text('Forma Corta'),
+            Center(
+              child: Image.network(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                width: 150,
+                height: 150,
+                fit: BoxFit.cover,
               ),
-              const Text('Forma Corta'),
-              Center(
-                child: Image.network(
-                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-                  width: 150,
-                  height: 150,
-                  fit: BoxFit.cover,
-                ),
+            ),
+            Center(
+              child: Image.asset(
+                'images/tiger.png', // Add image in asset section in pubspec.yaml.
+                width: 150,
+                height: 150,
+                fit: BoxFit.scaleDown,
               ),
-              Center(
-                child: Image.asset(
-                  'images/tiger.png', // Add image in asset section in pubspec.yaml.
-                  width: 150,
-                  height: 150,
-                  fit: BoxFit.scaleDown,
-                ),
-              ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     ),
   );
 }
